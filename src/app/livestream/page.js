918b7890603b6581c5e78872ebefc406b602a2a0
Naoki Hatano <ikoanonatah.1119@yahoo.ne.jp>
2023-08-03 15:04:42 +0900

    Remove unnecessary code

--- src/app/livestream/page.js
+++ src/app/livestream/page.js
@@ -2,17 +2,6 @@ import './livestream.css';
 import Script from 'next/script'
 
 const Page = async () => {
-    const settings = {
-        twitch: {
-            clientId: "e5ilv5ag1mc7uja1794dvrjq5sdeq4",
-            clientSecret: "nqfih1mzempt52jljv2v0flo2z356v",
-            gameId: "2692",
-        },
-        youtube: {
-            key: "AIzaSyBKYQBlmrZ6M8pd6Btr-Ebecxru9h2cAok",
-            query: "マリオサンシャイン",
-        }
-    };
 
     ///// Twitch /////
 
@@ -56,8 +45,11 @@ const Page = async () => {
         )
 
         return (
-            <div className="streams">
-                {twitchStreams}
+            <div>
+                <h1>Twitchで配信中 {streams.length}件</h1>
+                <div className="streams">
+                    {twitchStreams}
+                </div>
             </div>
         );
     }
@@ -79,7 +71,7 @@ const Page = async () => {
         "Client-Id": process.env.TWITCH_CLIENT_ID,
         };
 
-        const res = await fetch(endpoint, { headers });
+        const res = await fetch(endpoint, { headers, next: { revalidate: 30 } });
         const data = await res.json();
         return renderStreams(data);
     }
@@ -88,98 +80,12 @@ const Page = async () => {
 
     ///// YouTube /////
 
-    const googleApiClientReady = function() {
-        console.log("googleApiClientReady");
-        gapi.auth.init(function() {
-            window.setTimeout(checkAuth, 1);
-        });
-    }
-
-    function checkAuth() {
-        console.log("checkAuth");
-        gapi.auth.authorize({
-            client_id: OAUTH2_CLIENT_ID,
-            scope: OAUTH2_SCOPES,
-            immediate: true
-        }, handleAuthResult);
-    }
-
-    function handleAuthResult(authResult) {
-        console.log("handleAuthResult");
-        if (authResult && !authResult.error) {
-            // Authorization was successful. Hide authorization prompts and show
-            // content that should be visible after authorization succeeds.
-            $('.pre-auth').hide();
-            $('.post-auth').show();
-            loadAPIClientInterfaces();
-        } else {
-            // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
-            // client flow. The current function is called when that flow completes.
-            $('#login-link').click(function() {
-                gapi.auth.authorize({
-                client_id: OAUTH2_CLIENT_ID,
-                scope: OAUTH2_SCOPES,
-                immediate: false
-                }, handleAuthResult);
-            });
-        }
-    }
-
-    function loadAPIClientInterfaces() {
-        console.log("loadAPIClientInterfaces");
-        gapi.client.load('youtube', 'v3', function() {
-            handleAPILoaded();
-        });
-    }
-
-    function handleAPILoaded() {
-        console.log("handleAPILoaded");
-        //$('#search-button').attr('disabled', false);
-        search();
-    }
-
-    function search() {
-        console.log("search");
-        const { key, query } = settings.youtube;
-        var request = gapi.client.youtube.search.list({
-            q: query,
-                key: key,
-            part: 'snippet',
-                eventType: 'live',
-                type: 'video',
-                maxResults: 50,
-        });
-      
-        request.execute(function(response) {
-            console.log(JSON.stringify(response.result));
-            str = '';
-            const { items } = response.result;
-            if (items.length) {
-                items.forEach (item => {
-                    const { videoId } = item.id;
-                    const { title, thumbnails, channelTitle } = item.snippet;
-                    str += `
-                    <div class="stream-container">
-                        <a href=https://www.youtube.com/live/${videoId}" target="_blank" rel="noopener noreferrer">
-                            <img src="${thumbnails.medium.url}" />
-                            <h2>${title}</h2>
-                            <p>${channelTitle}</p>
-                        </a>
-                </div>`;
-                });
-            }
-            //$('#search-container').html(str);
-        });
-    }
-
     const { key, query } = settings.youtube;
     let data = null;
     try {
-        const res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&maxResults=10&key=${process.env.YOUTUBE_KEY}&q=${process.env.YOUTUBE_QUERY}`);
+        const res = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&eventType=live&type=video&maxResults=10&key=${process.env.YOUTUBE_KEY}&q=${process.env.YOUTUBE_QUERY}`, { next: { revalidate: 30 } });
         data = await res.json();
-        console.log("test1");
         console.log(data);
-        console.log("test2");
     } catch (error) {
         console.log(error);
     }
@@ -204,16 +110,17 @@ const Page = async () => {
     }
 
     const youtubeContainer = 
-        <div className="streams">
-            {youtubeStreams}
+        <div>
+            <h1>YouTubeで配信中 {items.length}件</h1>
+            <div className="streams">
+                {youtubeStreams}
+            </div>
         </div>;
 
     return (
         <div>
-            <h1>Twitchで配信中</h1>
             {twitchContainer}
             <br /><br />
-            <h1>YouTubeで配信中</h1>
             {youtubeContainer}
             
         </div>
